96c45c3fb2dec04eb565360d09c0ad4c08504092
ChainsawGarden <71266769+ChainsawGarden@users.noreply.github.com>
2021-11-16 15:47:29 -0500

    try fixing the virtual overload err by removing const param declaration

--- src/mapgen.h
+++ src/mapgen.h
@@ -418,7 +418,7 @@ class mapgen_function_lua : public virtual mapgen_function
         mapgen_function_lua( std::string s, int w = 1000 ) : mapgen_function( w ), scr( s ) {
             // scr = s; // @todo: if ( luaL_loadstring(L, scr.c_str() ) ) { error }
         }
-        void generate( map *, const oter_id &, const mapgendata &, const time_point &, float ) override; // @todo generate doesn't override anything, look into this
+        void generate( map *, const oter_id &, mapgendata &, const time_point &, float ) override; // @todo generate doesn't override anything, look into this
         //void generate( map *, const oter_id &, const mapgendata &, const time_point &, float ); // this should work since generate isn't being overrode.
 };
 // lua bloc end
