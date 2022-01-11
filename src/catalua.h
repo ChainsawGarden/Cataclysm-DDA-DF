68742bcf2cf8702086560dd447ada3710b707af8
ChainsawGarden <71266769+ChainsawGarden@users.noreply.github.com>
2022-01-11 14:10:07 -0500

    put the class back in the .cpp for now

--- src/catalua.h
+++ src/catalua.h
@@ -134,6 +134,7 @@ extern std::stringstream lua_output_stream; // string fstream for general output
 extern std::stringstream lua_error_stream; // string fstream for errors?
 
 #ifdef LUA // second lua section; outside of the CallbackArgument struct.
+
     using overmap_direction = om_direction::type; // alias for overmap direction type
     int luah_store_in_registry( lua_State *L, int stackpos );
     void luah_remove_from_registry( lua_State *L, int item_index );
@@ -143,72 +144,6 @@ extern std::stringstream lua_error_stream; // string fstream for errors?
     bool lua_report_error( lua_State *L, int err, const char *path, bool simple );
 
     void update_globals( lua_State *L );
-
-    class lua_iuse_wrapper : public iuse_actor {
-        private:
-            int lua_function;
-         public:
-        lua_iuse_wrapper( const int f, const std::string &type ) : iuse_actor( type ), lua_function( f ) {}
-        ~lua_iuse_wrapper() override = default;
-        // long use( player &, item &it, bool a, const tripoint &pos ) const override { // used to have "const override"
-        cata::optional<int> use( player &, item &it, bool a, const tripoint &pos ) const override { // used to have "const override"
-                                                                                              // on second thought, we might have to use `optional<long>`
-
-            // We'll be using lua_state a lot!
-            lua_State *const L = lua_state;
-
-            // If it's a lua function, the arguments have to be wrapped in
-            // lua userdata's and passed on the lua stack.
-            // We will now call the function f(player, item, active)
-
-            update_globals( L );
-
-            // Push the lua function on top of the stack
-            lua_rawgeti( L, LUA_REGISTRYINDEX, lua_function );
-
-            // TODO: also pass the player object, because of NPCs and all
-            //       I guess
-
-            // Push the item on top of the stack.
-            const int item_in_registry = LuaReference<item>::push_reg( L, it );
-            // Push the "active" parameter on top of the stack.
-            lua_pushboolean( L, a );
-            // Push the location of the item.
-            const int tripoint_in_registry = LuaValue<tripoint>::push_reg( L, pos );
-
-            // Call the iuse function
-            int err = lua_pcall( L, 3, 1, 0 );
-            lua_report_error( L, err, "iuse function" );
-
-            // Make sure the now outdated parameters we passed to lua aren't
-            // being used anymore by setting a metatable that will error on
-            // access.
-            luah_remove_from_registry( L, item_in_registry );
-            luah_setmetatable( L, "outdated_metatable" );
-            luah_remove_from_registry( L, tripoint_in_registry );
-            luah_setmetatable( L, "outdated_metatable" );
-
-            return lua_tointeger( L, -1 );
-        }
-
-        std::unique_ptr<iuse_actor> clone() const override;
-
-        //iuse_actor *clone() const override { // used to be a pointer
-        // std::unique_ptr<iuse_actor> clone() const override { // All the cool kids use "std::unique_ptr<typehere>" now.
-        //     // pimp our return
-        //     return std::make_unique<lua_iuse_wrapper> lua_iuse_wrapper( *this );
-        //     // return new lua_iuse_wrapper( *this ); old return
-
-        //     // what we're going to end up doing is make an external function that does this; the class method will call that function.
-        // }
-
-        //void load( JsonObject & ) override {}
-        void load( const JsonObject & ) override {} // "probably doesn't need an override"... was my old thoughts. Now I'm thinking something a little different.
-                                                    // all of the other `iuse`s in the `iuse_actor.h` use a JsonObject param defined as `const JsonObject &obj`.
-                                                    // if this succeeds, then this would mean that ALL overrides previously removed from Lua code are simply missing the
-                                                    // properly-defined params.
-    };
-    
 
     //void Item_factory::register_iuse_lua( const std::string &name, int lua_function );
 
