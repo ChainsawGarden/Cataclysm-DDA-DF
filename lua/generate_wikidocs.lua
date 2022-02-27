db56d2df783f07cd1580cdf060b1cf014ec9e330
ChainsawGarden <71266769+ChainsawGarden@users.noreply.github.com>
2022-02-27 17:06:50 -0500

    modernize wikidocs (turn into markdown doc)

--- lua/generate_wikidocs.lua
+++ lua/generate_wikidocs.lua
@@ -1,10 +1,10 @@
 dofile "class_definitions.lua"
 
-io.write("=== Global Functions ===\n")
+io.write("# Global Functions\n")
 
 function write_function(name, func)
     io.write("* ")
-    io.write("'''"..(func.rval or "void").."'''")
+    io.write("`"..(func.rval or "void").."`")
     io.write(" ")
     io.write(name)
     io.write("(")
@@ -20,22 +20,22 @@ function write_function(name, func)
     io.write(")\n")
     
     if func.desc then
-        io.write("** "..func.desc.."\n")
+        io.write("> "..func.desc.."\n")
     end
 end
 
 function write_attribute(name, attribute)
-    io.write("* ")
-    io.write("'''"..attribute.type.."'''")
+    io.write("- ")
+    io.write("`"..attribute.type.."`")
     io.write(" ")
     io.write(name)
     if not attribute.writable then
-        io.write(" ''read-only''")
+        io.write(" `read-only`")
     end
     io.write("\n")
     
     if attribute.desc then
-        io.write("** "..attribute.desc.."\n")
+        io.write("> "..attribute.desc.."\n")
     end
 end
 
@@ -44,14 +44,14 @@ for name, func in pairs(global_functions) do
 end
 
 for classname, class in pairs(classes) do
-    io.write("=== "..classname.." ===\n")
+    io.write("## "..classname.."\n")
     
-    io.write("==== Functions ====\n")
+    io.write("### Functions\n")
     for name, func in pairs(class.functions) do
         write_function(name, func)
     end
     
-    io.write("==== Attributes ====\n")
+    io.write("### Attributes\n")
     for name, attr in pairs(class.attributes) do
         write_attribute(name, attr)
     end
