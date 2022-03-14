67fe8e2d104d0a38e52f47c193d14148111b433e
ChainsawGarden <71266769+ChainsawGarden@users.noreply.github.com>
2022-03-14 19:02:08 -0400

    fix missing parenthesis

--- src/lua_console.cpp
+++ src/lua_console.cpp
@@ -9,8 +9,8 @@
 
 // lua_console::lua_console() : cWin( catacurses::newwin( lines, width, 0, 0 ) ),
 lua_console::lua_console() : 
-    cWin( catacurses::newwin( lines, width, point_zero ),
-    iWin( catacurses::newwin( 1, width, struct point_zero ) )
+    cWin( catacurses::newwin( lines, width, point_zero ) ),
+    iWin( catacurses::newwin( 1, width, point_zero ) )
 {
     #ifndef LUA
         text_stack.push_back( {_( "This build does not support Lua." ), c_red} );
