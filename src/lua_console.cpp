0e95a51898b10fc883c57940d9553d1686c0f56a
ChainsawGarden <71266769+ChainsawGarden@users.noreply.github.com>
2022-03-27 19:24:21 -0400

    Try fixing console

--- src/lua_console.cpp
+++ src/lua_console.cpp
@@ -20,8 +20,9 @@ lua_console::lua_console() :
         text_stack.push_back( {_( "Press [Esc] to close the Lua console." ), c_blue} );
 }
 
-lua_console::~lua_console() = default;
+lua_console::~lua_console() = default; // Console destructor
 
+// Handles input meant for the console; currently handles Escape, and N/P PAGE buttons.
 std::string lua_console::get_input()
 {
     std::map<long, std::function<bool()>> callbacks {
@@ -48,8 +49,7 @@ std::string lua_console::get_input()
         } };
     string_input_popup popup;
     // popup.window( iWin, 0, 0, width )
-    // popup.window(iWin, point_zero, width)
-    popup.window(iWin, {0, 10}, width) // this new code should put the input console at the bottom of the window
+    popup.window(iWin, point_zero, width)
     .max_length( width )
     .identifier( "LUA" );
     popup.callbacks = callbacks;
@@ -57,37 +57,43 @@ std::string lua_console::get_input()
     return popup.text();
 }
 
+// Draws text in the cWin window.
 void lua_console::draw()
 {
     werase( cWin );
 
     // Some juggling to make sure text is aligned with the bottom of the console.
-    int stack_size = text_stack.size() - scroll;
-    for( int i = lines; i > lines - stack_size && i >= 0; i-- ) {
-        auto line = text_stack[stack_size - 1 - ( lines - i )];
-        mvwprintz( cWin, point(i - 1, 0), line.second, line.first );
+    int stack_size = text_stack.size() - scroll; // get the text stack size
+    for( int i = lines; i > lines - stack_size && i >= 0; i-- ) { // iterate through the text stack
+        auto line = text_stack[stack_size - 1 - ( lines - i )]; // get the line
+        // mvwprintz( cWin, point(i - 1, 0), line.second, line.first ); // print the line
+        mvwprintz( cWin, point(0, i-1), line.second, line.first ); // print the line
     }
 
     wrefresh( cWin );
 }
 
+// Sets "done" to True.
 void lua_console::quit()
 {
     done = true;
 }
 
+// Scrolls up the console.
 void lua_console::scroll_down()
 {
     scroll = std::min( std::max( ( static_cast<int>( text_stack.size() ) ) - lines, 0 ), scroll + 1 );
     draw();
 }
 
+// Scrolls down the console.
 void lua_console::scroll_up()
 {
     scroll = std::max( 0, scroll - 1 );
     draw();
 }
 
+// Reads from the console's input.
 void lua_console::read_stream( std::stringstream &stream, nc_color text_color )
 {
     std::string line;
@@ -100,6 +106,7 @@ void lua_console::read_stream( std::stringstream &stream, nc_color text_color )
     stream.clear();
 }
 
+// Reads input and executes Lua
 void lua_console::run()
 {
     while( !done ) {
@@ -107,12 +114,12 @@ void lua_console::run()
 
         std::string input = get_input();
 
-#ifdef LUA
+#ifdef LUA // if Lua is enabled.
         call_lua( input );
 
         read_stream( lua_output_stream, c_white );
         read_stream( lua_error_stream, c_red );
-#else
+#else // if Lua is not enabled.
         text_stack.push_back( {_( "This build does not support Lua." ), c_red} );
         text_stack.push_back( {_( "Press [Esc] to close the Lua console." ), c_blue} );
 #endif // LUA
